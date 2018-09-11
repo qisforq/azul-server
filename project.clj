dd150c82dfb98865f2e932c7de6557a3e4a217a9
Jesse Sherlock <jesse@jessesherlock.com>
2018-09-11 11:05:26 -0700

    dependency updates
    - io.grpc versions updated
    - protobuf-tcp-rpc removed
    - clj-time -> clojure.java-time (use new java time rather than joda)
    - GRPC deps
    - honeysql for pg talk, removed dynamo deps
    - integrant for service management
    - timbre for logging

--- project.clj
+++ project.clj
@@ -4,24 +4,33 @@
   :source-paths      ["src/clj"]
   :java-source-paths ["src/java"]
   :main venezuela.core
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clj-time "0.14.4"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [clojure.java-time "0.3.2"]
 
                  ;; RPC
                  [com.google.protobuf/protobuf-java "3.6.1"]
-                 [com.athaydes.protobuf/protobuf-tcp-rpc "0.2.1"]
+
+                 ;; GRPC
+                 [io.grpc/grpc-netty "1.4.0"]
+                 [io.grpc/grpc-protobuf "1.4.0"]
+                 [io.grpc/grpc-stub "1.4.0"]
+
+                 ;; SQL
+                 [honeysql "0.9.3"]
 
                  ;; Bitcoin
                  [org.bitcoinj/bitcoinj-core "0.14.7"]
                  [org.lightningj/lightningj "0.4.2-Beta-2"]
 
-                 ;; Persistence
-                 [com.taoensso/faraday "1.9.0"]
-                 [jstrutz/hashids "1.0.1"]
-
                  ;; Push
                  [kemurisense/twijio "0.1.1"]
                  [com.google.gcm/gcm-server "1.0.0"]
+
+                 ;; Services
+                 [integrant "0.6.3"]
+
+                 ;; Logging
+                 [com.taoensso/timbre "4.10.0"]
 
                  ])
 

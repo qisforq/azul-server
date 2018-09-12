744454bf90b5dd1e0282155ab6001e03e13425ab
Fenton Travers <fenton.travers@gmail.com>
2018-09-11 22:53:58 -0400

    basic insert/query on users table

--- src/clj/venezuela/db/migrations.clj
+++ src/clj/venezuela/db/migrations.clj
@@ -4,10 +4,14 @@
 (def config
   {:store :database
    :db {:connection-uri
-        "jdbc:postgresql://localhost/azuldb?user=azul&password="}}) 
+        "jdbc:postgresql://localhost/azuldb_dev?user=azul&password="}})
 
 (comment
+  ;; run the following to run all migrations
   (migratus/migrate config)
+
+  ;; each rollback just goes 1 migration backwards, executed multiple times to
+  ;; return to 0
   (migratus/rollback config))
 
 

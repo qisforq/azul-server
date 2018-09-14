30d361f8abfce913bc55bcb4128f7a5a219c91eb
Ben Woosley <ben.woosley@gmail.com>
2018-09-14 11:54:07 -0400

    Run rpc/client_test on port 5002
    So as not to conflict with the dev server, which
    runs on port 5001 currently.

--- test/venezuela/rpc/client_test.clj
+++ test/venezuela/rpc/client_test.clj
@@ -4,19 +4,20 @@
             [clojure.test :as t :refer [deftest is testing]]))
 
 
-(deftest login-test
-  (let [server (server/start-server 5001)]
+(deftest rpc-client-test
+  (let [server (server/start-server 5002)]
     (try
-      (let [client (client/make-client 5001)]
-        (testing "successful path"
-          (let [username "me"
-                password "12345"
-                response (client/login client username password)]
-            (is (= {:success true :session-token "12345"} response))))
-        (testing "failure path"
-          (let [username "me"
-                password "abcde"
-                response (client/login client username password)]
-            (is (= {:success false :message "bad password"} response)))))
+      (let [client (client/make-client 5002)]
+        (testing "login"
+          (testing "successful path"
+            (let [username "me"
+                  password "12345"
+                  response (client/login client username password)]
+              (is (= {:success true :session-token "12345"} response))))
+          (testing "failure path"
+            (let [username "me"
+                  password "abcde"
+                  response (client/login client username password)]
+              (is (= {:success false :message "bad password"} response))))))
       (finally
         (server/stop-server server)))))

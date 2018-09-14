17db924a0ef9b2809ed0478d9df58395ecc7d615
Jesse Sherlock <jesse@jessesherlock.com>
2018-09-14 08:19:50 -0700

    Added shutdown hook to main
    `lein run` or `java -jar uberjar.jar` will run the server on port 8080
    
    TODO: proper config/defaults rather than hardcoded

--- src/clj/venezuela/core.clj
+++ src/clj/venezuela/core.clj
@@ -1,6 +1,12 @@
 (ns venezuela.core
+  (:gen-class)
   (:require
    [venezuela.rpc.server :refer [start-server]]))
 
-(defn -main []
-  (start-server))
+(defn -main
+  [& args]
+  (let [shutdown-promise (promise)]
+    (.addShutdownHook (Runtime/getRuntime)
+                      (Thread. (fn [] (deliver shutdown-promise :done))))
+    (start-server 8080)
+    @shutdown-promise))

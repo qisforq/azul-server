7f3045220238484fa71c335f49993df5c54e5318
Ben Woosley <ben.woosley@gmail.com>
2018-09-19 17:19:46 -0400

    Add validation constraints and testing to user registration

--- src/clj/venezuela/auth/registration.clj
+++ src/clj/venezuela/auth/registration.clj
@@ -5,16 +5,22 @@
             [java-time :as time]))
 
 (defn register [username password]
-  (if (db/user-by-username db/db {:username username})
-    {:success false :message "User already exists"}
-
-  (let [salt (str (hash (time/local-time)))
-        salted-password  (str password salt)
-        hashed-password (hashers/derive salted-password)
-        user-id (-> (db/create-user db/db {:username username
-                            :hashed_password hashed-password :salt salt})
-                      first
-                      :id)]
-        {:success true :session-token (sessions/create user-id hashed-password)}
-    )
-))
+  (cond
+    (empty? username)
+      {:success false :message "Username is required"}
+    (> 3 (count username))
+      {:success false :message "Username is too short, min length: 3"}
+    (empty? password)
+      {:success false :message "Password is required"}
+    (db/user-by-username db/db {:username username})
+      {:success false :message "User already exists"}
+    :else
+      (let [salt (str (hash (time/local-time)))
+            salted-password  (str password salt)
+            hashed-password (hashers/derive salted-password)
+            user-id (-> (db/create-user db/db {:username username
+                                :hashed_password hashed-password :salt salt})
+                          first
+                          :id)]
+            {:success true :session-token (sessions/create user-id hashed-password)}
+        )))
--- test/venezuela/rpc/client_test.clj
+++ test/venezuela/rpc/client_test.clj
@@ -9,23 +9,54 @@
   (let [server (server/start-server 5002)]
     (try
       (let [client (client/make-client 5002)]
+        (testing "registration"
+          (testing "when username is blank it fails"
+            (let [username ""
+                  password "12345"
+                  registration (client/register client username password)]
+              (is (= {:success false :message "Username is required"} registration))))
+          (testing "when username is short it fails"
+            (let [username "me"
+                  password "12345"
+                  registration (client/register client username password)]
+              (is (= {:success false :message "Username is too short, min length: 3"} registration))))
+          (testing "when username is taken it fails"
+            (let [username "user"
+                  password "12345"
+                  existing_registration (client/register client username password)
+                  registration (client/register client username password)]
+              (is (= {:success false :message "User already exists"} registration))))
+          (testing "when password is blank it fails"
+            (let [username "user"
+                  password ""
+                  registration (client/register client username password)]
+              (is (= {:success false :message "Password is required"} registration))))
+          (testing "when password is valid and username is available it passes"
+            (let [username "user"
+                  password "12345"
+                  registration (client/register client username password)]
+              (comment "FIXME: this will not work until the test database is cleared with each run"
+                (is (:success registration))
+                (is (not (string/blank? (:session-token registration)))))
+              (is (not (:success registration))))))
+
         (testing "login"
           (testing "when user exists and password is correct it succeeds"
-            (let [username "me"
+            (let [username "user"
                   password "12345"
                   registration (client/register client username password)
                   response (client/login client username password)]
               (is (:success response))
               (is (not (string/blank? (:session-token response))))))
           (testing "when user exists and password is incorrect it fails"
-            (let [username "me"
+            (let [username "user"
                   password "12345"
                   incorrect_password "23456"
                   registration (client/register client username password)
                   response (client/login client username incorrect_password)]
               (is (= {:success false :message "Invalid password"} response))))
           (testing "when user doesn't exist it fails"
-            (let [username "me"
+            (let [username "user"
                   password "abcde"
                   response (client/login client username password)]
               (comment "FIXME: this will not work until the test database is cleared with each run"
